bff87556a69371c95305a0b5f18f85a529fcc0fc
Petro Sapronov <saprik13@gmail.com>
2022-07-31 22:58:17 +0300

    hw7 ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+// Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+function createGallery(galleryItems, galleryCb) {
+  const addImages = galleryItems
+    .map(
+      (image) => `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"/></a></div>`
+    )
+    .join('');
+
+  galleryCb.insertAdjacentHTML('afterbegin', addImages);
+}
+createGallery(galleryItems, galleryList);
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+  const isGalleryImage = e.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGallery(galleryItems, galleryCb) {
+  const addImages = galleryItems
+    .map(
+      (image) => `
+          <a class="gallery__item" href="${image.original}">
+          <img class="gallery__image"
+          src="${image.preview}" 
+          alt="${image.description}" />
+</a>
+  `
+    )
+    .join('');
+  galleryCb.insertAdjacentHTML('afterbegin', addImages);
+}
+createGallery(galleryItems, galleryList);
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+  const isGalleryImage = e.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+
+  let lightbox = new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
